8aa97b81dc21db28872c59ca22a8ac61db8b1ef6
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 17:47:36 +0800

    f\

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express  =require('express')
 const path = require('path')
-const cors=require("cors");
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static(path.resolve('./build')))
-const corsOptions ={
-    origin:'*', 
-    credentials:true,           
-    optionSuccessStatus:200,
- }
 
-app.use(cors(corsOptions))
 
 app.get('*',(_,res)=>{
     res.sendFile(path.resolve('./build/index.html'))
