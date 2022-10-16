bfa8f4c1ea23b731a2617579685aad8ee7e34733
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 19:26:52 +0800

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./assets/google-style.css";
 import { InitialBackroundComponent } from "./features/shared/presentation/components/initial-backround-component";
+import { useInterceptorAxios } from "./lib/interceptor-axios";
 
 const App: React.FC = (): JSX.Element => {
+  const { isAuthenticated } = useInterceptorAxios();
   return (
     <React.Fragment>
       <InitialBackroundComponent />
