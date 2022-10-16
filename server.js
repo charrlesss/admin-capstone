afbce4ca255ff7c622e87856a774c7abfccf3f02
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 17:00:05 +0800

    s

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express  =require('express')
-const bodyParser = require('body-Parser')   
 const path = require('path')
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(express.static(path.join(__dirname,'/build')))
 
 app.get('*',(_,res)=>{
