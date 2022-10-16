ca68b4c05ed5b03763302c6899ef593b4f5e789e
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 17:10:52 +0800

    s

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'/build')))
+app.use(express.static(path.resolve('./build')))
 
 app.get('*',(_,res)=>{
-    res.sendFile(path.join(__dirname, './build','/index.html'))
+    res.sendFile(path.resolve('./build/index.html'))
 })
 
 const PORT = process.env.PORT||5500
