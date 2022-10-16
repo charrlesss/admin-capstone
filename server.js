f5bfff15be9da77a03d4da9cffbe45a61c2192ca
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 16:32:44 +0800

    f

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express  =require('express')
+const bodyParser = require('body-Parser')   
 const path = require('path')
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'/build')))
 
 app.get('*',(_,res)=>{
     res.sendFile(path.join(__dirname,'/build/index.html'))
 })
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT||5500
 
 app.listen(PORT ,()=>{
     console.log('listen in port ' + PORT)
