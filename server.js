f2d3b4d8f6d6fd0dde800f4050cb0380d4a316fe
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 17:53:30 +0800

    f

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express  =require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.static(path.resolve('./build')))
+app.use(express.json());
+app.use(express.static(path.resolve("./build")));
 
+app.get("*", (_, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  res.sendFile(path.resolve("./build/index.html"));
+});
 
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve('./build/index.html'))
-})
+const PORT = process.env.PORT || 5500;
 
-const PORT = process.env.PORT||5500
-
-app.listen(PORT ,()=>{
-    console.log('listen in port ' + PORT)
-})
-
+app.listen(PORT, () => {
+  console.log("listen in port " + PORT);
+});
